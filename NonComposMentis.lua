f1232cd549d1dfd962e786b3dae3c8cbdc15c119
Cal Henderson <cal@iamcal.com>
2010-03-04 23:50:51 +0000

    update frame when player finishes loaded and when any rep changes. format huge numbers with a comma. show DMF and BB rep.
    git-svn-id: https://svn.iamcal.com/public/wow/NonComposMentis/trunk@5116 ac7c8259-0de0-4f3c-bb6a-12394caf8efd

--- NonComposMentis.lua
+++ NonComposMentis.lua
@@ -51,6 +51,14 @@ function NCM.OnEvent(frame, event, ...)
 
 	if (event == 'LOOT_CLOSED') then
 	end
+
+	if (event == 'PLAYER_ENTERING_WORLD') then
+		NCM.UpdateFrame();
+	end	
+
+	if (event == 'CHAT_MSG_COMBAT_FACTION_CHANGE') then
+		NCM.UpdateFrame();
+	end
 end
 
 function NCM.StartFrame()
@@ -94,6 +102,19 @@ function NCM.Toggle()
 	end
 end
 
+function NCM.FormatNumber(n)
+
+	local s = string.format("%d", n);
+
+	if (string.len(s) > 4) then
+
+		local l = string.len(s);
+		s = string.sub(s, 1, l - 3) .. ',' .. string.sub(s, l - 2);
+	end
+
+	return s;
+end
+
 function NCM.UpdateFrame()
 
 	-- update text here...
@@ -103,6 +124,10 @@ function NCM.UpdateFrame()
 	local reps = NCM.GetReps();
 
 
+	--
+	-- Ravenholdt
+	--
+
 	local rh = reps["Ravenholdt"] or 0;
 	local rh_remain = 42000 - rh;
 	if (rh_remain < 1) then
@@ -110,7 +135,7 @@ function NCM.UpdateFrame()
 		txt = txt .. "Ravenholdt: DONE!\n";
 	else
 		txt = txt .. "Ravenholdt\n";
-		txt = txt .. indent .. rh_remain .. "rep remaining\n";
+		txt = txt .. indent .. NCM.FormatNumber(rh_remain) .. " rep remaining\n";
 
 		-- can we still kill members?
 		if (rh < 21000) then
@@ -123,6 +148,74 @@ function NCM.UpdateFrame()
 		local rh_boxes = math.ceil(rh_remain / 75) * 5;
 		txt = txt .. indent .. "Junk boxes to finish: " .. rh_boxes .. "\n";
 	end
+	txt = txt .. "\n";
+
+
+	--
+	-- Darkmoon Faire
+	--
+
+	local dmf = reps["Darkmoon Faire"] or 0;
+	local dmf_remain = 42000 - dmf;
+	if (dmf_remain < 1) then
+
+		txt = txt .. "Darkmoon Faire: DONE!\n";
+	else
+		txt = txt .. "Darkmoon Faire\n";
+		txt = txt .. indent .. NCM.FormatNumber(dmf_remain) .. " rep remaining\n";
+
+		if (dmf < 5000) then
+			local dmf_quests = math.ceil((5000 - dmf) / 250);
+			txt = txt .. indent .. "Quests until maxed: " .. dmf_quests .. "\n";
+		end
+
+		local dmf_minor = math.ceil(dmf_remain / 25);
+		local dmf_major = math.ceil(dmf_remain / 350);
+
+		txt = txt .. indent .. "Minor decks to finish: " .. dmf_minor .. "\n";
+		txt = txt .. indent .. "Major decks to finish: " .. dmf_major .. "\n";
+	end
+	txt = txt .. "\n";
+
+
+	--
+	-- Shen'dralar
+	--
+
+	local sd = reps["Shen'dralar"] or 0;
+	local sd_remain = 42000 - sd;
+	if (sd_remain < 1) then
+
+		txt = txt .. "Shen'dralar: DONE!\n";
+	else
+		txt = txt .. "Shen'dralar\n";
+		txt = txt .. indent .. NCM.FormatNumber(sd_remain) .. " rep remaining\n";
+	end
+	txt = txt .. "\n";
+
+
+	--
+	-- Bloodsail Buccaneers
+	--
+
+	-- new default is 35500/36000 hostile, which is 6500 below 0/3000 neutral
+
+	local bb = reps["Bloodsail Buccaneers"] or -6500;
+	local bb_remain = 9000 - bb;
+	if (bb_remain < 1) then
+
+		txt = txt .. "Bloodsail Buccaneers: DONE!\n";
+	else
+		txt = txt .. "Bloodsail Buccaneers\n";
+		txt = txt .. indent .. NCM.FormatNumber(bb_remain) .. " rep remaining\n";
+
+		local bb_bruisers = math.ceil(bb_remain / 25);
+		txt = txt .. indent .. "Bruiser kills to finish: " .. bb_bruisers .. "\n";
+	end
+
+
+
+
 	
 
 	if (false) then
@@ -201,5 +294,7 @@ NCM.Frame:RegisterEvent("PLAYER_TARGET_CHANGED")
 NCM.Frame:RegisterEvent("LOOT_OPENED")
 NCM.Frame:RegisterEvent("LOOT_CLOSED")
 NCM.Frame:RegisterEvent("PLAYER_LOGOUT")
+NCM.Frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+NCM.Frame:RegisterEvent("CHAT_MSG_COMBAT_FACTION_CHANGE")
 
 NCM.OnLoad()
